12897c76b1276f503f4a3ee0914c7bf4b83be413
Freddy Lundekvam <freddy@lundekvam.info>
2021-01-14 19:54:11 +0100

    Syntax cleanup

--- axios.js
+++ axios.js
@@ -4,16 +4,15 @@ const axiosRetry = require('axios-retry')
 const axios = _axios.create()
 
 const retryDelay = (retryNumber = 0) => {
-    const seconds = Math.pow(2, retryNumber) * 1000
-    const randomMs = 1000 * Math.random()
-    return seconds + randomMs
-};
+  const seconds = Math.pow(2, retryNumber) * 1000
+  const randomMs = 1000 * Math.random()
+  return seconds + randomMs
+}
 
 axiosRetry(axios, {
-    retries: 2,
-    retryDelay,
-    // retry on Network Error & 5xx responses
-    retryCondition: axiosRetry.isRetryableError,
+  retries: 2,
+  retryDelay,
+  retryCondition: axiosRetry.isRetryableError
 })
 
 module.exports = axios
