53d263b815058cdcdd6b0daffc9c42cd5c43e847
Freddy Lundekvam <freddy@lundekvam.info>
2021-01-18 08:29:17 +0100

    Syntax cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import vue from 'rollup-plugin-vue';
-import commonjs from 'rollup-plugin-commonjs';
+import vue from 'rollup-plugin-vue'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
-    entry: 'index.js',
-    plugins: [
-        commonjs(),
-        vue()
-    ]
+  entry: 'index.js',
+  plugins: [
+    commonjs(),
+    vue()
+  ]
 }
